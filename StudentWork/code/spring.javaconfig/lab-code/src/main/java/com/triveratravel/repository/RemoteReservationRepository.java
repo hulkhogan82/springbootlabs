6c000b152f0ba13e4b7193f7de09c1091f78809a
Hulk Hogan <thehulkhogan@yahoo.com>
2020-08-25 10:43:47 -0400

    Initial one day 2

---
+++ StudentWork/code/spring.javaconfig/lab-code/src/main/java/com/triveratravel/repository/RemoteReservationRepository.java
@@ -0,0 +1,15 @@
+package com.triveratravel.repository;
+
+import com.triveratravel.model.Reservation;
+import com.triveratravel.repository.ReservationRepository;
+import com.triveratravel.repository.stub.TestData;
+
+import java.util.List;
+
+public class RemoteReservationRepository implements ReservationRepository {
+
+    @Override
+    public List<Reservation> findAll() {
+        return TestData.testData();
+    }
+}
