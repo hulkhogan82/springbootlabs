b7a416dc47ced6cb73f868c6797ae43d91d769dc
Hulk Hogan <thehulkhogan@yahoo.com>
2020-08-27 23:00:23 -0400

    Lab8 Spring Data JPA ueing Spring Boot Part2 completed

--- StudentWork/code/spring-data-jpa-boot/lab-code/src/main/java/com/triveratravel/JavaConfig.java
+++ StudentWork/code/spring-data-jpa-boot/lab-code/src/main/java/com/triveratravel/JavaConfig.java
@@ -1,21 +1,26 @@
 package com.triveratravel;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:datasource.properties")
 public class JavaConfig {
 
     @Bean
+    @ConfigurationProperties("travel.datasource")
     public DataSource dataSource() {
-        DataSourceBuilder<?> builder = DataSourceBuilder.create();
+        /*DataSourceBuilder<?> builder = DataSourceBuilder.create();
         builder.driverClassName("org.apache.derby.jdbc.ClientDriver")
                 .username("triveratravel")
                 .password("password")
                 .url("jdbc:derby://localhost:50505/Hotel");
-        return builder.build();
+         */
+        return DataSourceBuilder.create().build();
     }
 }
